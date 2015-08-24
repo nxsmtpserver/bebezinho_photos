7b0a856d6e98622c81f5cab50833618eec2e7b8d
Francisco Luz <franciscoferreiraluz@yahoo.com.au>
2015-08-24 11:27:06 -0300

    Update...

--- push.sh
+++ push.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-cp -r ~/Pictures/nx_photos/png_original/128/done ~/Pictures/bebezinho/nx_photos/fotos
-cp -r ~/Pictures/nx_photos/descricao/128/done ~/Pictures/bebezinho/nx_photos/descricao
-cp -r ~/Pictures/nx_photos/displays/128 ~/Pictures/bebezinho/nx_photos/displays
+cp -r ~/Pictures/nx_photos/png_original/128/done/* ~/Pictures/bebezinho/nx_photos/fotos
+cp -r ~/Pictures/nx_photos/descricao/128/done/* ~/Pictures/bebezinho/nx_photos/descricao
+cp -r ~/Pictures/nx_photos/displays/128/* ~/Pictures/bebezinho/nx_photos/displays
 
 cd ~/Pictures/bebezinho/nx_photos
 git add --all
